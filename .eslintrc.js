ed7f60a3782415f864f2581a3ae0efb37453424c
sg <info@sg-medien.com>
2016-12-22 00:45:21 +0100

    add socket support

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,15 +11,7 @@ module.exports = {
   "rules": {
     "valid-typeof": "off",
     "no-underscore-dangle": "off",
-    "max-len": [
-      1,
-      150,
-      2,
-      { ignoreComments: true }
-    ],
-    "no-param-reassign": [
-      "error",
-      { "props": false }
-    ]
+    "max-len": [1, 150, 2, { ignoreComments: true }],
+    "no-param-reassign": ["error",{ "props": false }]
   }
 };
